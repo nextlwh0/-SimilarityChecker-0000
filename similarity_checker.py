f503fe1b88f968e13f295b4ef7094c4c17a8c47f
whlee <nextlwh@daum.net>
2024-06-13 16:04:04 +0900

    [refactoring] method extraction for same length or far-different length of inputs

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,10 +1,17 @@
 class SimilarityChecker:
     def check_similarity(self, str_a, str_b):
         self.assert_illegal_input(str_a, str_b)
-        if len(str_a) == len(str_b):
+        if self.is_same_length(str_a, str_b):
             return 60
-        if len(str_a) <= len(str_b):
+        if self.is_longer_than_twice_length(str_a, str_b):
             return 0
+
+
+    def is_longer_than_twice_length(self, str_a, str_b):
+        return len(str_a) <= len(str_b) or len(str_a) >= len(str_b)
+
+    def is_same_length(self, str_a, str_b):
+        return len(str_a) == len(str_b)
 
     def assert_illegal_input(self, str_a, str_b):
         if str_a is None or str_b is None:
