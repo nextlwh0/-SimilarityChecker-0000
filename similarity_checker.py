0c729a8727c48aa31ba056c232dd85bebe997a48
whlee <nextlwh@daum.net>
2024-06-13 15:58:02 +0900

    [refactoring] test code refactoring for setup. product code refactoring for illegal input

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,6 +1,9 @@
 class SimilarityChecker:
     def check_similarity(self, str_a, str_b):
+        self.assert_illegal_input(str_a, str_b)
+
+    def assert_illegal_input(self, str_a, str_b):
         if str_a is None or str_b is None:
             raise TypeError()
-        if len(str_a)==0 or len(str_b)==0:
+        if len(str_a) == 0 or len(str_b) == 0:
             raise TypeError()
--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -3,13 +3,16 @@ from unittest import TestCase
 from similarity_checker import SimilarityChecker
 
 
-class TestChcker(TestCase):
-    def test_exception_when_input_is_none(self):
+class TestChecker(TestCase):
+    def setUp(self):
         self.checker = SimilarityChecker()
+        super().setUp()
+
+    def test_exception_when_input_is_none(self):
         with self.assertRaises(TypeError):
             self.checker.check_similarity()
 
     def test_exception_when_input_length_is_zero(self):
-        self.checker = SimilarityChecker()
         with self.assertRaises(TypeError):
             self.checker.check_similarity("", "")
+
